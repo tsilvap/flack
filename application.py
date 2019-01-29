ee6f3b1b13e27e568e3621e7660b9d7dc1c4e602
Thiago da Silva <thiagodasilva@protonmail.com>
2019-01-29 15:17:21 -0200

    Better data structures for channels

--- application.py
+++ application.py
@@ -1,44 +1,72 @@
 """The very best Slack clone written in Flask."""
 import os
 
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, redirect, render_template, request, url_for
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 socketio = SocketIO(app)
 
-channel_list = []
+
+class Channel(object):
+    """A channel object."""
+
+    def __init__(self, channel_name):
+        self.name = channel_name
+        self.messages = []
 
 
-@app.route("/")
+class ChannelList(object):
+    """List of channels singleton."""
+
+    def __init__(self):
+        self.list = []
+
+    def append(self, channel):
+        """Append a channel to the list."""
+        self.list.append(channel)
+
+    def names(self):
+        """Return list of channel names."""
+        return [channel.name for channel in self.list]
+
+
+channel_list = ChannelList()
+
+
+@app.route('/')
 def index():
     """Index page, prompts for a display name in first visit."""
-    return render_template("index.html", channels=channel_list)
+    return render_template('index.html', channel_names=channel_list.names())
 
 
-@app.route("/create_channel", methods=["POST"])
+@app.route('/create_channel', methods=['POST'])
 def create_channel():
     """Create channel function."""
-    channel_name = request.form.get("channel-name")
+    channel_name = request.form.get('channel-name')
 
-    if channel_name in channel_list:
-        return jsonify({"success": False})
+    if channel_name in channel_list.names():
+        return jsonify({'success': False})
 
-    channel_list.append(channel_name)
-    return jsonify({"success": True})
+    channel = Channel(channel_name)
+    channel_list.append(channel)
+    return jsonify({'success': True})
 
 
-@app.route("/channel/<string:channel_name>")
+@app.route('/channel/<string:channel_name>')
 def channel(channel_name):
     """Channel page."""
     # TODO: Check if channel exists and handle page appropriately
-    if channel_name not in channel_list:
-        pass
+    if channel_name not in channel_list.names():
+        return redirect(url_for('index'))
 
     return render_template(
-        "channel.html", channels=channel_list, channel_name=channel_name)
+        'channel.html',
+        channel_names=channel_list.names(),
+        channel_name=channel_name
+    )
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     socketio.run(app)
