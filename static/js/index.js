4ed5966c629e1c6b5df0bb16280e0bf2400afe14
Yura <yura@localhost.localdomain>
2019-01-29 09:08:01 -0200

    Integrate ESLint in project

--- static/js/index.js
+++ static/js/index.js
@@ -1,33 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Wire up create channel button.
-    document.querySelector('#create-channel').onclick = () => {
-        // Toggle create channel form visibility.
-        const display = document.querySelector('#create-channel-form').style.display;
+  // Wire up create channel button.
+  document.querySelector('#create-channel').onclick = () => {
+    const form = document.querySelector('#create-channel-form');
 
-        display = display === 'block' ? 'none' : 'block';
+    form.style.display = form.style.display === 'block' ? 'none' : 'block';
+  };
+
+  // Wire up create channel form.
+  document.querySelector('#create-channel-form').onsubmit = () => {
+    const channelName = document.querySelector('#channel-name').value;
+    const xhr = new XMLHttpRequest();
+    const data = new FormData();
+
+    xhr.onload = () => {
+      const res = JSON.parse(xhr.responseText);
+
+      if (!res.success) {
+        // Display error message.
+        document.querySelector('#channel-name-taken').style.display = 'block';
+      } else {
+        // Otherwise, redirect to channel page.
+        window.location.href = `/channel/${channelName}`;
+      }
     };
 
-    // Wire up create channel form.
-    document.querySelector('#create-channel-form').onsubmit = () => {
-        const channelName = document.querySelector('#channel-name').value;
-        const xhr = new XMLHttpRequest();
-        const data = new FormData();
+    xhr.open('POST', '/create_channel');
+    data.append('channel-name', channelName);
+    xhr.send(data);
 
-        xhr.onload = function () {
-            const data = JSON.parse(this.responseText);
-            // Display error message.
-            if (!data.success) {
-                document.querySelector('#channel-name-taken').style.display = 'block';
-            }
-            // Otherwise, redirect to channel page.
-            else {
-                window.location.href = `/channel/${channelName}`;
-            }
-        };
-        xhr.open('POST', '/create_channel');
-        data.append('channel-name', channelName);
-        xhr.send(data);
-        return false; // Prevent default behavior.
-    };
-
+    return false; // Prevent default behavior.
+  };
 });
