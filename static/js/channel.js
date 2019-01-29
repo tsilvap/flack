19d454c864e41c0319c2885c181bdd52c56c4c39
Thiago da Silva <thiagodasilva@protonmail.com>
2019-01-29 21:33:34 -0200

    Sending messages implementation working

--- static/js/channel.js
+++ static/js/channel.js
@@ -23,17 +23,41 @@ document.addEventListener('DOMContentLoaded', () => {
       const xhr = new XMLHttpRequest();
 
       xhr.onload = () => {
-<<<<<<< HEAD
-        const messages = JSON.parse(xhr.responseText);
+        // Get messages.
+        const msgs = JSON.parse(xhr.responseText);
 
+        // Clear list of messages.
+        const msgsElem = document.querySelector('#messages');
+        while (msgsElem.firstChild) {
+          msgsElem.removeChild(msgsElem.firstChild);
+        }
 
-        // TODO: Display received messages.
-=======
-        // TODO: Handle received messages.
-        const messages = JSON.parse(xhr.responseText);
+        // (Re)render all messages in the DOM.
+        msgs.forEach((msg) => {
+          const messageElem = document.createElement('li');
+          const firstDiv = document.createElement('div');
+          const secondDiv = document.createElement('div');
+          const authorElem = document.createElement('span');
+          const dateElem = document.createElement('small');
 
-        console.log(messages);
->>>>>>> b806bd02bfc1fcb614cefccf04a55ecdc178df41
+          // Set up first div.
+          authorElem.className = 'author';
+          authorElem.innerHTML = msg.displayName;
+          dateElem.className = 'date';
+          dateElem.innerHTML = ` ${msg.date}`; // Space for display purposes.
+          firstDiv.appendChild(authorElem);
+          firstDiv.appendChild(dateElem);
+
+          // Set up second div.
+          secondDiv.innerHTML = msg.messageText;
+
+          // Append divs to the message element.
+          messageElem.appendChild(firstDiv);
+          messageElem.appendChild(secondDiv);
+
+          // Append message element to the list of messages.
+          msgsElem.appendChild(messageElem);
+        });
       };
       xhr.open('GET', `/messages/${channelName}`);
       xhr.send();
