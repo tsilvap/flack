0429f2dfafa454446fe87f872ed0fb8327470e79
Yura <yura@localhost.localdomain>
2019-01-29 06:43:24 -0200

    Reorganize project and improve build pipeline

--- application.py
+++ application.py
@@ -1,7 +1,8 @@
+"""The very best Slack clone written in Flask."""
 import os
 
 from flask import Flask, jsonify, render_template, request
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
